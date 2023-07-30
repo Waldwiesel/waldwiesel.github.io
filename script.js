f8904808b837517b40f4c78b08de435e902caadd
Waldwiesel <henagon3@gmail.com>
2023-07-30 19:31:47 +0200

    Starting JS to add Button functionality

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+var verhaltnisformel_button = document.getElementById("verhaltnisformel_button")
+var summenformel_button = document.getElementById("summenformel_button")
 
+verhaltnisformel_button.addEventListener("click", showVerhaltnisformel)
+
+function showVerhaltnisformel() {
+    var content_window = document.getElementById("content-window")
+}
